11597f342568d878657b909889d78f27f5020888
lakshmihugar <94183813+lakshmihugar@users.noreply.github.com>
2021-12-07 16:03:44 +0530

    adding code change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { CreateaccountComponent } from './components/createaccount/createaccount.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',component:HomeComponent
+  },{
+    path:'Aboutus',component:AboutusComponent
+  },
+  {
+    path:'Contactus',component:ContactusComponent
+  },
+  {
+    path:'Login',component:LoginComponent
+  },
+  {
+    path:'Createaccount',component:CreateaccountComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateaccountComponent } from './components/createaccount/createaccount.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    LoginComponent,
+    CreateaccountComponent
   ],
   imports: [
     BrowserModule,
